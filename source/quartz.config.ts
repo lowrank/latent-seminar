463ccb94dc162dd8d9445031d99c80de77e16081
Yimin Zhong <yzz0225@auburn.edu>
2025-06-17 00:18:10 -0500

    update

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "lowrank.github.io/latent-seminar",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
--- source/quartz.layout.ts
+++ source/quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/lowrank/latent-seminar",
+      "Personal Website": "https://aub.ie/yimin",
     },
   }),
 }
